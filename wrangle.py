0e2724740aa8d2065e2c1b31702e2ab31382e54c
Yvette Ibarra <yvette.ibarra01@gmail.com>
2022-12-01 12:33:06 -0600

    update split function

--- wrangle.py
+++ wrangle.py
@@ -329,21 +329,34 @@ def rename_columns(df):
 
 ###################################### Split Data
 
-def split_data(df):
+def split(df, target_var):
     '''
-    split_data takes in data Frame and splits into  train , validate, test.
-    The split is 20% test 80% train/validate. Then 30% of 80% validate and 70% of 80% train.
-    Aproximately (train 56%, validate 24%, test 20%)
-    Returns train, validate, and test 
+    This function takes in the dataframe and target variable name as arguments and then
+    splits the dataframe into train (56%), validate (24%), & test (20%)
+    It will return a list containing the following dataframes: train (for exploration), 
+    X_train, X_validate, X_test, y_train, y_validate, y_test
     '''
-    # split test data from train/validate
-    train_and_validate, test = train_test_split(df, random_state=123, test_size=.2)
+    # split df into train_validate (80%) and test (20%)
+    train_validate, test = train_test_split(df, test_size=.20, random_state=13)
+    # split train_validate into train(70% of 80% = 56%) and validate (30% of 80% = 24%)
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=13)
 
-    # split train from validate
-    train, validate = train_test_split(train_and_validate, random_state=123, test_size=.3)
-                                   
-    return train, validate, test
+    # create X_train by dropping the target variable 
+    X_train = train.drop(columns=[target_var])
+    # create y_train by keeping only the target variable.
+    y_train = train[[target_var]]
 
+    # create X_validate by dropping the target variable 
+    X_validate = validate.drop(columns=[target_var])
+    # create y_validate by keeping only the target variable.
+    y_validate = validate[[target_var]]
+
+    # create X_test by dropping the target variable 
+    X_test = test.drop(columns=[target_var])
+    # create y_test by keeping only the target variable.
+    y_test = test[[target_var]]
+
+    return train, X_train, X_validate, X_test, y_train, y_validate, y_test
 
 
 ######################### SCALE SPLIT #########################
